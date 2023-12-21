f365e2c8d8b949b4171625035a9e9f5bddc67498
teri-epi <teri.epi@kakaoenterprise.com>
2023-12-21 14:53:05 +0900

    gRPC proto buf source

---
+++ src/main/java/com/kakao/www/AppArchiGuideGrpcProtoBufApplication.java
@@ -0,0 +1,11 @@
+package com.kakao.www;
+
+import org.springframework.boot.SpringApplication;
+
+public class AppArchiGuideGrpcProtoBufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppArchiGuideGrpcProtoBufApplication.class, args);
+    }
+
+}
