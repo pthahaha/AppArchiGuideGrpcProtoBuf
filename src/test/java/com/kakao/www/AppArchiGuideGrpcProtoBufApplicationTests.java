459e814b2f7f797a3077cb90e1ad0e2940804a8b
teri-epi <teri.epi@kakaoenterprise.com>
2024-01-13 15:49:22 +0900

    jpa관련 가격 float -> int64로 변경

---
+++ src/test/java/com/kakao/www/AppArchiGuideGrpcProtoBufApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.kakao.www;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AppArchiGuideGrpcProtoBufApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
